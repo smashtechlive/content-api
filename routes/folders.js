fe5b0806f275c950c26d4777496df3f4ea01a16c
Admin <brandon@smashtech.com>
2018-01-09 10:34:56 -0800

    create and read routes for folders, videos

--- routes/folders.js
+++ routes/folders.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../lib/config');
 
-router.get('/', function(req, res) {
-	res.send('FOLDERS index route');
+// CREATE
+router.post('/', function(req, res) {
+	res.send('FOLDERS create route');
 });
+
+// READ
+router.get('/', function(req, res) {
+
+	let recordLimit = req.query.limit || 50; 
+
+	config.db.collection('folders').find({}).limit(recordLimit).toArray(function (err, folders) {
+		let payload = {data: folders};
+		res.send(payload)
+	});
+
+});
+
+// UPDATE
+// DELETE
+
 
 module.exports = router;
--- routes/videos.js
+++ routes/videos.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); 
+const config = require('../lib/config');
 
-router.get('/', function(req, res) {
-	res.send('VIDEOS index route');
+// CREATE
+router.post('/', function(req, res) {
+	res.send('Videos create route');
 });
+
+// READ
+router.get('/', function(req, res) {
+
+	let recordLimit = req.query.limit || 50; 
+
+	config.db.collection('videos').find({}).limit(recordLimit).toArray(function (err, videos) {
+		let payload = {data: videos};
+		res.send(payload)
+	});
+
+});
+
+// UPDATE
+// DELETE
 
 module.exports = router;
